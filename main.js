ac0a9daecd76d12d39fba57f0c88f424dd7688eb
Aidan Elm <elmaidan@gmail.com>
2020-03-19 16:22:33 -0400

    add favicon (windows)

--- main.js
+++ main.js
@@ -5,11 +5,9 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+      width: 800,
+      height: 600,
+      icon: `${__dirname}/images/favicon.ico`
   })
 
   // and load the index.html of the app.
